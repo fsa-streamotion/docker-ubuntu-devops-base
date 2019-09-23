f791514f75593d7f87f624d4187aa5df5afe8852
Sajid <sajid.moinuddin@kayosports.com.au>
2019-09-23 19:42:13 +1000

    Adding jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,40 @@
+pipeline {
+  agent {
+    label "dockerhub-maven"
+  }
+  environment {
+    CHART_REPOSITORY= 'https://artifactory.cluster.foxsports-gitops-prod.com.au/artifactory/helm' // Please do not edit this line! Managed by customize.sh
+    ORG = 'fsa-streamotion' 
+    APP_NAME = 'ubuntu-devops-base'
+    DOCKER_REGISTRY='kayosportsau'
+  }
+  stages {
+
+    stage('Push To DockerHub') {
+      steps {
+        container('maven') {
+              // ensure we're not on a detached head
+              sh "git config --global credential.helper store"
+              sh "jx step git credentials"
+
+              sh "echo \$(jx-release-version) > VERSION"
+              sh "jx step tag --version \$(cat VERSION)"
+
+              sh "export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml"
+              sh "export VERSION=latest && skaffold build -f skaffold.yaml"
+
+              script {
+                def buildVersion =  readFile "${env.WORKSPACE}/VERSION"
+                currentBuild.description = "${DOCKER_REGISTRY}/ubuntu-devops-base:$buildVersion"
+                currentBuild.displayName = "$buildVersion"
+              }          
+        }
+      }
+    }
+  }
+  post {
+        always {
+          cleanWs()
+        }
+  }
+}
