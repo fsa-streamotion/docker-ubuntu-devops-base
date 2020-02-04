71a31cb5b37c7d9f1e79ba8f67ca73628d3c1c0e
sajid-moinuddin <51370076+sajid-moinuddin@users.noreply.github.com>
2020-02-04 12:10:30 +1100

    Update busybox.sh

--- add/dev-cheats/busybox.sh
+++ add/dev-cheats/busybox.sh
@@ -1,2 +1,2 @@
-kubectl run -it --rm test --image=busybox:1.28 --restart=Never --command -- sh
+kubectl run -it --requests=cpu=100m,memory=256Mi --limits=cpu=100m,memory=256Mi  --rm test --image=busybox:1.28 --restart=Never --command -- sh
 
